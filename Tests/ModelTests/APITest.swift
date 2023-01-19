e39e780426a54679a4b1641f327ff0422ce8bb84
Hasnain Khan <khan.hasnain0@gmail.com>
2023-01-18 20:07:59 -0500

    commit apitests

--- Tests/ModelTests/APITest.swift
+++ Tests/ModelTests/APITest.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import _0230118_HasnainKhan_NYCSchools
 
-final class _0230118_HasnainKhan_NYCSchoolsTests: XCTestCase {
+final class APITest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
