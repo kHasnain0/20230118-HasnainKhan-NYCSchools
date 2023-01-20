da998bad18184d2c32093be667ea84767f90c65a
Hasnain Khan <khan.hasnain0@gmail.com>
2023-01-19 19:12:53 -0500

    high school

--- Application/Model/HighSchool.swift
+++ Application/Model/HighSchool.swift
@@ -19,6 +19,7 @@ class HighSchool {
     private var school_name: String
     private var location: String
     private var school_email: String
+    private var grades: Scores
     
     
     //Constructor, thrown exeception if nil value
@@ -28,21 +29,44 @@ class HighSchool {
             self.school_name = school_name
             self.location = location
             self.school_email = school_email
+            self.grades = Scores()
         } catch {throw MyError.runtimeError(nilError)}
         
     }
     //Getter Methods
-        public func getDbn() -> String {return self.dbn }
+    public func getDbn() -> String {return self.dbn }
     
-        public func getSchoolName() -> String {return self.school_name }
-        
-        public func getLocation() -> String {return self.location }
+    public func getSchoolName() -> String {return self.school_name }
     
-        public func getSchool_Email() -> String {return self.school_email }
+    public func getLocation() -> String {return self.location }
     
+    public func getSchool_Email() -> String {return self.school_email }
+    
+    //set and get high school scores
+    private struct Scores {
+        var sat_critical_reading_avg_score: String?
+        var sat_math_avg_score: String?
+        var sat_writing_avg_score: String?
+    }
+    
+    public func setSatMathScore(math: String?) throws {
+        if math != nil {self.grades.sat_math_avg_score = math}
+        else {throw MyError.runtimeError(nilError) }
+    }
+    
+    public func setSatReadinghScore(read: String?) throws {
+        if read != nil {self.grades.sat_critical_reading_avg_score = read}
+        else {throw MyError.runtimeError(nilError) }
+    }
+    
+    public func setSatWritingScore(write: String?) throws {
+        if write != nil {self.grades.sat_writing_avg_score = write}
+        else {throw MyError.runtimeError(nilError) }
     }
     
     
+    
+}
     
     
     
